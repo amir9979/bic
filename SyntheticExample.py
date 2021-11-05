8fb74534bb8b1533b1f23a23b479c2d006a3ad2e
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2021-11-05 11:16:17 +0200

    Update SyntheticExample.py

--- SyntheticExample.py
+++ SyntheticExample.py
@@ -114,7 +114,7 @@ def write_file():
 
 
 if __name__ == '__main__':
-    window_size = 1
+    window_size = 50
     ind = int(sys.argv[1])
     commits_start = ind * window_size
     commits_end = commits_start + window_size
